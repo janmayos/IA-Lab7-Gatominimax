67c8ef6d212add94056286ee32a6040dc207c93f
janmayos <jangasca5@gmail.com>
2024-10-22 22:10:17 -0600

    formato

--- gato4x4.py
+++ gato4x4.py
@@ -4,264 +4,282 @@ import time
 # Profundidad máxima para limitar el tiempo de búsqueda
 PROFUNDIDAD_MAXIMA = 6
 
+
 def crear_tablero():
-	tablero = [[" ", " ", " "," "],
-			[" ", " ", " "," "], 
-			[" ", " ", " "," "],
-			[" ", " ", " "," "]]
-	print_tablero(tablero)
-	return tablero
+    tablero = [[" ", " ", " ", " "],
+               [" ", " ", " ", " "],
+               [" ", " ", " ", " "],
+               [" ", " ", " ", " "]]
+    print_tablero(tablero)
+    return tablero
+
 
 def print_tablero(tablero):
-	matrix_length = len(tablero)
-	for i in range(matrix_length):
-		print(tablero[i])
+    matrix_length = len(tablero)
+    for i in range(matrix_length):
+        print(tablero[i])
+
 
 def solicitar_coordenadas():
-	while True:
-		fila = int(input("Ingresa la fila:"))
-		if fila >= 0 and fila <=3:
-			break
-		else:
-			print("Favor de ingresar un valor del 0-3")
+    while True:
+        fila = int(input("Ingresa la fila:"))
+        if fila >= 1 and fila <= 4:
+            break
+        else:
+            print("Favor de ingresar un valor del 1-4")
 
-	while True:
-		columna = int(input("Ingresa la columna:"))
-		if columna >= 0 and columna <=3:
-			break
-		else:
-			print("Favor de ingresar un valor del 0-3")
-	
-	return fila,columna
+    while True:
+        columna = int(input("Ingresa la columna:"))
+        if columna >= 1 and columna <= 4:
+            break
+        else:
+            print("Favor de ingresar un valor del 1-4")
 
-def rellenar_casilla(tablero,fila,columna,valor):
-	if tablero[fila][columna] == " ":
-		tablero[fila][columna]= valor
-		print_tablero(tablero)
-	else:
-		print("Casilla ocupada volver a seleccionar coordenadas")
-		fila,columna = solicitar_coordenadas()
-		rellenar_casilla(tablero,fila,columna,valor)
+    return fila-1, columna-1
 
-def eval_columnas(tablero,valor,matrix_length,matrix_length_column):
-	ban = False
-	for columna in range(matrix_length):
-		for fila in range(matrix_length_column):
-			if tablero[fila][columna] == " " or tablero[fila][columna] != valor:
-				ban=False
-				break
-			else: 
-				ban = True
-		if ban:
-			break
-	return ban
 
-def eval_filas(tablero,valor,matrix_length,matrix_length_column):
-	ban = False
-	for fila in range(matrix_length):
-		for columna in range(matrix_length_column):
-			if tablero[fila][columna] == " " or tablero[fila][columna] != valor:
-				ban=False
-				break
-			else: 
-				ban = True
-		if ban:
-			break
-	return ban
+def rellenar_casilla(tablero, fila, columna, valor):
+    if tablero[fila][columna] == " ":
+        tablero[fila][columna] = valor
+        print_tablero(tablero)
+    else:
+        print("Casilla ocupada volver a seleccionar coordenadas")
+        fila, columna = solicitar_coordenadas()
+        rellenar_casilla(tablero, fila, columna, valor)
 
-def eval_diagonal(tablero,valor):
-	ban = False
-	if tablero[0][0] == valor and tablero[1][1] == valor and tablero[2][2] == valor and tablero[3][3] == valor:
-		ban =  True
-	if tablero[0][3] == valor and tablero[1][2] == valor and tablero[2][1] == valor and tablero[3][0] == valor:
-		ban =  True
-		
-	return ban
+
+def eval_columnas(tablero, valor, matrix_length, matrix_length_column):
+    ban = False
+    for columna in range(matrix_length):
+        for fila in range(matrix_length_column):
+            if tablero[fila][columna] == " " or tablero[fila][columna] != valor:
+                ban = False
+                break
+            else:
+                ban = True
+        if ban:
+            break
+    return ban
+
+
+def eval_filas(tablero, valor, matrix_length, matrix_length_column):
+    ban = False
+    for fila in range(matrix_length):
+        for columna in range(matrix_length_column):
+            if tablero[fila][columna] == " " or tablero[fila][columna] != valor:
+                ban = False
+                break
+            else:
+                ban = True
+        if ban:
+            break
+    return ban
+
+
+def eval_diagonal(tablero, valor):
+    ban = False
+    if tablero[0][0] == valor and tablero[1][1] == valor and tablero[2][2] == valor and tablero[3][3] == valor:
+        ban = True
+    if tablero[0][3] == valor and tablero[1][2] == valor and tablero[2][1] == valor and tablero[3][0] == valor:
+        ban = True
+
+    return ban
 
 
 def eval_tablero(tablero):
-	matrix_length = len(tablero)
-	matrix_length_column = len(tablero[0])
-	if eval_columnas(tablero,"X",matrix_length,matrix_length_column):
-		return "X gano en columna"
-	if(eval_columnas(tablero,"O",matrix_length,matrix_length_column)):
-		return "O gano en columna"
-	if eval_filas(tablero,"X",matrix_length,matrix_length_column):
-		return "X gano en fila"
-	if(eval_filas(tablero,"O",matrix_length,matrix_length_column)):
-		return "O gano en fila"
-	if eval_diagonal(tablero,"X"):
-		return "X gano en diagonal"
-	if(eval_diagonal(tablero,"O")):
-		return "O gano en diagonal"
+    matrix_length = len(tablero)
+    matrix_length_column = len(tablero[0])
+    if eval_columnas(tablero, "X", matrix_length, matrix_length_column):
+        return "X gano en columna"
+    if (eval_columnas(tablero, "O", matrix_length, matrix_length_column)):
+        return "O gano en columna"
+    if eval_filas(tablero, "X", matrix_length, matrix_length_column):
+        return "X gano en fila"
+    if (eval_filas(tablero, "O", matrix_length, matrix_length_column)):
+        return "O gano en fila"
+    if eval_diagonal(tablero, "X"):
+        return "X gano en diagonal"
+    if (eval_diagonal(tablero, "O")):
+        return "O gano en diagonal"
 
-	return " "
+    return " "
 
 
 def esta_lleno(tablero):
-	matrix_length = len(tablero)
-	matrix_length_column = len(tablero[0])
-	for fila in range(matrix_length):
-		for columna in range(matrix_length_column):
-			if tablero[fila][columna] == " ":
-				return False
-	return True
+    matrix_length = len(tablero)
+    matrix_length_column = len(tablero[0])
+    for fila in range(matrix_length):
+        for columna in range(matrix_length_column):
+            if tablero[fila][columna] == " ":
+                return False
+    return True
+
 
 def es_ganador(tablero):
-	
-	mensaje = eval_tablero(tablero)
-	if esta_lleno(tablero):
-		print("Es un empate")
-	else:
-		if mensaje == " ":
-			return False
-		else:
-			print("El jugador " + mensaje)
-	return True
+
+    mensaje = eval_tablero(tablero)
+    if esta_lleno(tablero):
+        print("Es un empate")
+    else:
+        if mensaje == " ":
+            return False
+        else:
+            print("El jugador " + mensaje)
+    return True
 
 
-def generar_aleatorios_rellenar(tablero,valor):
-	while True:
-		fila = random.randint(0, 3)
-		columna = random.randint(0, 3)
-		if tablero[fila][columna] == " ":
-			tablero[fila][columna]= valor
-			print(fila,columna)
-			break
-	print_tablero(tablero)
+def generar_aleatorios_rellenar(tablero, valor):
+    while True:
+        fila = random.randint(0, 3)
+        columna = random.randint(0, 3)
+        if tablero[fila][columna] == " ":
+            tablero[fila][columna] = valor
+            print(fila, columna)
+            break
+    print_tablero(tablero)
+
 
 def iniciar_juego_gato():
-	tablero = crear_tablero()
-	while(True):
-		print("Turno de X")
-		fila,columna = solicitar_coordenadas()
-		print(fila,columna)
-		rellenar_casilla(tablero,fila,columna,"X")
-		if es_ganador(tablero):
-			break
-		#fila,columna = solicitar_coordenadas()
-		#rellenar_casilla(tablero,fila,columna,"O")
-		generar_aleatorios_rellenar(tablero,"0")
-		if es_ganador(tablero):
-			break
+    tablero = crear_tablero()
+    while (True):
+        print("Turno de X")
+        fila, columna = solicitar_coordenadas()
+        print(fila, columna)
+        rellenar_casilla(tablero, fila, columna, "X")
+        if es_ganador(tablero):
+            break
+        # fila,columna = solicitar_coordenadas()
+        # rellenar_casilla(tablero,fila,columna,"O")
+        generar_aleatorios_rellenar(tablero, "0")
+        if es_ganador(tablero):
+            break
 
-def iniciar_juego_gato_2_jugadores(): 
-	tablero = crear_tablero()
-	while(True):
-		fila,columna = solicitar_coordenadas()
-		print(fila,columna)
-		rellenar_casilla(tablero,fila,columna,"X")
-		if es_ganador(tablero):
-			break
-		#fila,columna = solicitar_coordenadas()
-		#rellenar_casilla(tablero,fila,columna,"O")
-		
-		print("Turno de O")
-		fila,columna = solicitar_coordenadas()
-		print(fila,columna)
-		rellenar_casilla(tablero,fila,columna,"O")
-		if es_ganador(tablero):
-			break
+
+def iniciar_juego_gato_2_jugadores():
+    tablero = crear_tablero()
+    while (True):
+        fila, columna = solicitar_coordenadas()
+        print(fila, columna)
+        rellenar_casilla(tablero, fila, columna, "X")
+        if es_ganador(tablero):
+            break
+        # fila,columna = solicitar_coordenadas()
+        # rellenar_casilla(tablero,fila,columna,"O")
+
+        print("Turno de O")
+        fila, columna = solicitar_coordenadas()
+        print(fila, columna)
+        rellenar_casilla(tablero, fila, columna, "O")
+        if es_ganador(tablero):
+            break
+
 
 def heuristica(tablero):
-	"""Evalúa el tablero para asignar un puntaje."""
-	if eval_tablero(tablero)[0] == "O":
-		return 10  # Gana la computadora
-	elif eval_tablero(tablero)[0] == "X":
-		return -10  # Gana el jugador
-	return 0  # Empate o tablero sin ganador
+    """Evalúa el tablero para asignar un puntaje."""
+    if eval_tablero(tablero)[0] == "O":
+        return 10  # Gana la computadora
+    elif eval_tablero(tablero)[0] == "X":
+        return -10  # Gana el jugador
+    return 0  # Empate o tablero sin ganador
 
-def mejor_movimiento(tablero,simbolo,es_maximizando):
+
+def mejor_movimiento(tablero, simbolo, es_maximizando):
     mejor_puntaje = -float('inf')
     movimiento = None
     for i in range(4):
         for j in range(4):
             if tablero[i][j] == " ":
                 tablero[i][j] = simbolo
-                puntaje = minimax_alpha_beta(tablero, 0, es_maximizando, -float('inf'), float('inf'))
+                puntaje = minimax_alpha_beta(
+                    tablero, 0, es_maximizando, -float('inf'), float('inf'))
                 tablero[i][j] = " "
                 if puntaje > mejor_puntaje:
                     mejor_puntaje = puntaje
                     movimiento = (i, j)
     return movimiento
 
+
 def minimax_alpha_beta(tablero, profundidad, es_maximizando, alpha, beta):
-	#print_tablero(tablero)
-	ganador = eval_tablero(tablero)
-	if ganador == None or esta_lleno(tablero) or profundidad == PROFUNDIDAD_MAXIMA:
-		return heuristica(tablero)
-	
-	if es_maximizando:
-		max_eval = -float('inf')
-		for i in range(4):
-			for j in range(4):
-				if tablero[i][j] == " ":
-					tablero[i][j] = "O"
-					eval_alpha_beta = minimax_alpha_beta(tablero, profundidad + 1, False, alpha, beta)
-					tablero[i][j] = " "
-					max_eval = max(max_eval, eval_alpha_beta)
-					alpha = max(alpha, eval_alpha_beta)
-					if beta <= alpha:
-						break  # Poda beta
-		return max_eval
-	else:
-		min_eval = float('inf')
-		for i in range(4):
-			for j in range(4):
-				if tablero[i][j] == " ":
-					tablero[i][j] = "X"
-					eval_alpha_beta = minimax_alpha_beta(tablero, profundidad + 1, True, alpha, beta)
-					tablero[i][j] = " "
-					min_eval = min(min_eval, eval_alpha_beta)
-					beta = min(beta, eval_alpha_beta)
-					if beta <= alpha:
-						break  # Poda alpha
-		return min_eval
+    # print_tablero(tablero)
+    ganador = eval_tablero(tablero)
+    if ganador == None or esta_lleno(tablero) or profundidad == PROFUNDIDAD_MAXIMA:
+        return heuristica(tablero)
 
-def iniciar_juego_gato_ia_ia(): 
-	tablero = crear_tablero()
-	while(True):
-		print("Turno de X")
-		fila,columna = mejor_movimiento(tablero,"X",True)
-		print(fila,columna)
-		rellenar_casilla(tablero,fila,columna,"X")
-		time.sleep(1)
-		if es_ganador(tablero):
-			break
-		#fila,columna = solicitar_coordenadas()
-		#rellenar_casilla(tablero,fila,columna,"O")
-		
-		print("Turno de O")
-		fila,columna = mejor_movimiento(tablero,"O",False)
-		print(fila,columna)
-		rellenar_casilla(tablero,fila,columna,"O")
-		time.sleep(1)
-		if es_ganador(tablero):
-			break
-
-def iniciar_juego_gato_jugador_ia(): 
-	tablero = crear_tablero()
-	while(True):
-		print("Turno de X")
-		fila,columna = solicitar_coordenadas()
-		print(fila,columna)
-		rellenar_casilla(tablero,fila,columna,"X")
-		if es_ganador(tablero):
-			break
-		#fila,columna = solicitar_coordenadas()
-		#rellenar_casilla(tablero,fila,columna,"O")
-		
-		print("Turno de O")
-		fila,columna = mejor_movimiento(tablero,"O",False)
-		print(fila,columna)
-		rellenar_casilla(tablero,fila,columna,"O")
-		if es_ganador(tablero):
-			break
+    if es_maximizando:
+        max_eval = -float('inf')
+        for i in range(4):
+            for j in range(4):
+                if tablero[i][j] == " ":
+                    tablero[i][j] = "O"
+                    eval_alpha_beta = minimax_alpha_beta(
+                        tablero, profundidad + 1, False, alpha, beta)
+                    tablero[i][j] = " "
+                    max_eval = max(max_eval, eval_alpha_beta)
+                    alpha = max(alpha, eval_alpha_beta)
+                    if beta <= alpha:
+                        break  # Poda beta
+        return max_eval
+    else:
+        min_eval = float('inf')
+        for i in range(4):
+            for j in range(4):
+                if tablero[i][j] == " ":
+                    tablero[i][j] = "X"
+                    eval_alpha_beta = minimax_alpha_beta(
+                        tablero, profundidad + 1, True, alpha, beta)
+                    tablero[i][j] = " "
+                    min_eval = min(min_eval, eval_alpha_beta)
+                    beta = min(beta, eval_alpha_beta)
+                    if beta <= alpha:
+                        break  # Poda alpha
+        return min_eval
 
 
-if __name__ == "__main__": 
-	#iniciar_juego_gato()
-	#iniciar_juego_gato_2_jugadores()
-	#iniciar_juego_gato_jugador_IA()
-	#iniciar_juego_gato_jugador_IA
-	iniciar_juego_gato_ia_ia()
+def iniciar_juego_gato_ia_ia():
+    tablero = crear_tablero()
+    while (True):
+        print("Turno de X")
+        fila, columna = mejor_movimiento(tablero, "X", True)
+        print(fila, columna)
+        rellenar_casilla(tablero, fila, columna, "X")
+        time.sleep(1)
+        if es_ganador(tablero):
+            break
+        # fila,columna = solicitar_coordenadas()
+        # rellenar_casilla(tablero,fila,columna,"O")
+
+        print("Turno de O")
+        fila, columna = mejor_movimiento(tablero, "O", False)
+        print(fila, columna)
+        rellenar_casilla(tablero, fila, columna, "O")
+        time.sleep(1)
+        if es_ganador(tablero):
+            break
+
+
+def iniciar_juego_gato_jugador_ia():
+    tablero = crear_tablero()
+    while (True):
+        print("Turno de X")
+        fila, columna = solicitar_coordenadas()
+        print(fila, columna)
+        rellenar_casilla(tablero, fila, columna, "X")
+        if es_ganador(tablero):
+            break
+        # fila,columna = solicitar_coordenadas()
+        # rellenar_casilla(tablero,fila,columna,"O")
+
+        print("Turno de O")
+        fila, columna = mejor_movimiento(tablero, "O", False)
+        print(fila, columna)
+        rellenar_casilla(tablero, fila, columna, "O")
+        if es_ganador(tablero):
+            break
+
+
+if __name__ == "__main__":
+    # iniciar_juego_gato()
+    # iniciar_juego_gato_2_jugadores()
+    # iniciar_juego_gato_jugador_IA()
+    # iniciar_juego_gato_jugador_IA
+    iniciar_juego_gato_ia_ia()
--- menu.py
+++ menu.py
@@ -1,30 +1,31 @@
 
-from gato4x4 import iniciar_juego_gato_jugador_ia,iniciar_juego_gato_ia_ia,iniciar_juego_gato_2_jugadores
+from gato4x4 import iniciar_juego_gato_jugador_ia, iniciar_juego_gato_ia_ia, iniciar_juego_gato_2_jugadores
 
 
 def menu():
-	print("*** MENU  GATO 4x4***")
-	print("1.-Humano vs Humano")
-	print("2.-Humano vs IA")
-	print("3.-IA vs IA")
-	print("4.-Salir")
-	while True:
-		opcion = int(input("Ingresa la opción deseada entre 1-4:"))
-		if opcion >= 1 and opcion <=4:
-			break
-		else:
-			print("Valor incorrecto")
-	return opcion
+    print("*** MENU  GATO 4x4***")
+    print("1.-Humano vs Humano")
+    print("2.-Humano vs IA")
+    print("3.-IA vs IA")
+    print("4.-Salir")
+    while True:
+        opcion = int(input("Ingresa la opción deseada entre 1-4:"))
+        if opcion >= 1 and opcion <= 4:
+            break
+        else:
+            print("Valor incorrecto")
+    return opcion
 
-if __name__ == "__main__": 
-	while True:
-		opcion = menu()
-		if opcion == 1:
-			iniciar_juego_gato_2_jugadores()
-		elif opcion == 2:
-			iniciar_juego_gato_jugador_ia()
-		elif opcion == 3:
-			iniciar_juego_gato_ia_ia()
-		else:
-			print("Gracias por jugar")
-			exit(0)
+
+if __name__ == "__main__":
+    while True:
+        opcion = menu()
+        if opcion == 1:
+            iniciar_juego_gato_2_jugadores()
+        elif opcion == 2:
+            iniciar_juego_gato_jugador_ia()
+        elif opcion == 3:
+            iniciar_juego_gato_ia_ia()
+        else:
+            print("Gracias por jugar")
+            exit(0)
